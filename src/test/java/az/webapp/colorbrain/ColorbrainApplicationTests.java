5ab78e92abbb462770571023f10c865e56f36db9
Elmir Z. Tagiyev <Elmir.Tagiyev@kapitalbank.az>
2021-03-21 14:53:16 +0400

    initial commit

---
+++ src/test/java/az/webapp/colorbrain/ColorbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package az.webapp.colorbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
