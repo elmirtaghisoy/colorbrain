9e51ca91fdbf926fa8837397ae17dcdb70f824a1
Elmir Z. Tagiyev <Elmir.Tagiyev@kapitalbank.az>
2021-03-25 03:39:52 +0400

    Training module exception handling and adding ckeditor

--- src/main/java/az/webapp/colorbrain/controller/TrainingController.java
+++ src/main/java/az/webapp/colorbrain/controller/TrainingController.java
@@ -5,10 +5,14 @@ import az.webapp.colorbrain.service.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/training")
@@ -26,7 +30,6 @@ public class TrainingController {
     @GetMapping("/finished")
     public String getAllFinishedTraining(Model model) {
         model.addAttribute("trainings", trainingService.getAllFinishedTraining());
-        model.addAttribute("rowCounter", 0L);
         return "admin/allTrainingPage";
     }
 
@@ -35,17 +38,25 @@ public class TrainingController {
             @PathVariable("id") TrainingEntity trainingEntity,
             Model model
     ) {
-        model.addAttribute("training", trainingEntity);
+        model.addAttribute("trainingEntity", trainingService.getOneTrainingById(trainingEntity.getId()));
         return "admin/oneTrainingPage";
     }
 
     @GetMapping("/create")
-    public String getCreatePage() {
+    public String getCreatePage(Model model) {
+        model.addAttribute("trainingEntity", new TrainingEntity());
         return "admin/createTrainingPage";
     }
 
     @PostMapping("/create")
-    public String saveTraining(TrainingEntity trainingEntity) {
+    public String saveTraining(
+            @Valid @ModelAttribute("trainingEntity") TrainingEntity trainingEntity,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "admin/createTrainingPage";
+        }
+        System.out.println(trainingEntity.toString());
         trainingService.saveTraining(trainingEntity);
         return "redirect:/training/active";
     }
@@ -58,8 +69,12 @@ public class TrainingController {
 
     @PostMapping("/update")
     public String updateTraining(
-            TrainingEntity trainingEntity
+            @Valid @ModelAttribute("trainingEntity") TrainingEntity trainingEntity,
+            BindingResult bindingResult
     ) {
+        if (bindingResult.hasErrors()) {
+            return "admin/oneTrainingPage";
+        }
         trainingService.updateTraining(trainingEntity);
         return "redirect:/training/active";
     }
